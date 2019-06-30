cb4bbb4fb9d21682fe2b25025b4aaac2d2cc19de
Matheus Donato Pereira <mano_matheusblu@hotmail.com>
2019-06-30 17:18:40 -0300

    Criado Class de Conexao dentro da pasta DataBase

---
+++ Repository/DataBase/Conexao.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.DataBase
+{
+    public class Conexao
+    {
+        public static SqlCommand AbrirConexao()
+        {
+            SqlConnection conexao = new SqlConnection();
+            conexao.ConnectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            conexao.Open();
+
+            SqlCommand comando = new SqlCommand();
+            comando.Connection = conexao;
+            return comando;
+        }
+    }
+}
