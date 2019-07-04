6b1daa6a8f7448d6cb43a5f08ac10e45fe65b55f
Matheus Donato Pereira <mano_matheusblu@hotmail.com>
2019-07-04 14:52:09 -0300

    Resolvido erro do Database2

--- Repository/DataBase/Estrutura.sql
+++ Repository/DataBase/Estrutura.sql
@@ -7,6 +7,7 @@ DROP TABLE IF EXISTS categorias;
 DROP TABLE IF EXISTS contas_pagar;
 DROP TABLE IF EXISTS contas_receber;
 
+
 CREATE TABLE contabilidades(
 	id INT PRIMARY KEY IDENTITY(1,1),
 	nome VARCHAR(45) NOT NULL
